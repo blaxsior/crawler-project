625129797d938a32091ce36384bb4aea8606a052
blaxsior <heejun2798@gmail.com>
2023-08-31 17:13:57 +0900

    chore: prisma 등의 의존성 추가로 인한 설정 변경

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,23 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function getQueryStrings(params: {key: string, value: string}[]) {
+  const searchParams = new URLSearchParams();
+  for(const {key, value} of params) {
+    searchParams.set(key, value);
+  }
+
+  return searchParams.toString();
+}
+
+export const colors = [
+  '#FF6384',
+  '#36a2eb',
+  '#ff6384',
+  '#ff9f40',
+  '#ffcd56',
+  '#4bc0c0',
+  '#9966ff',
+  '#f5b7f7'
+];
